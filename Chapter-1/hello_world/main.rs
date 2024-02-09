cc20995ab3f1f6efc922cc9bbfdd6baa34368498
Robert Stevenson <78357805+robert-stevenson-1@users.noreply.github.com>
2024-02-09 21:37:00 +0000

    Chapter 1 of The Book

---
+++ Chapter-1/hello_world/main.rs
@@ -0,0 +1,14 @@
+// main function that is the first point of entry into the program
+fn main() {
+    // 'println!' is a macro that prints the string to the console
+    // '!' is used to to indicate that this is a macro
+    println!("hello, world!");
+}
+
+// compile the program with:
+// `rustc main.rs`
+//
+// run the program with:
+// `./main` or `./main.exe`
+// 
+// Windows will also output a debug information in the `.pdb` file
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Rust in Code
  "Rust in Code" is a play on "Rest in Piece" for my stubbon mind and wanting to avoid learning Rust, but here I am trying it out
+
+## Resources used
+
+[_The Book..._](https://doc.rust-lang.org/book/title-page.html) - The Rust Programming Language
